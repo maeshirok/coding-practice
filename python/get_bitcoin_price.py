d19d6fa158c8f3ea6cef0ee577fd1945f7b0e9f3
maeshirok <aosspresto@gmail.com>
2023-08-06 20:08:16 +0900

    add file

---
+++ python/get_bitcoin_price.py
@@ -0,0 +1,16 @@
+"""
+CoinGecko APIを利用してbitcoinの価格を取得するプログラムです。
+"""
+import requests
+  
+url = "https://api.coingecko.com/api/v3"
+endpoint = "/simple/price"
+url = url + endpoint
+params = {"ids":"bitcoin","vs_currencies":"jpy,usd"}
+
+response = requests.request("GET", url, params=params)
+r = response.json()
+
+print("1BTCの価格")
+print(str(r['bitcoin']['jpy']) + "円")
+print(str(r['bitcoin']['usd']) + "ドル")
