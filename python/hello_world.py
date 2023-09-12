ee52d84ed95303cfeb6471630387e62892356e90
maeshirok <aosspresto@gmail.com>
2023-09-12 21:14:19 +0900

    add: Hello World on ruby

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,12 +1,6 @@
 """
 2023/08/11
-いろんなHelloWorld
+Hello World
 """
 
-a = "Hello"
-b = "World!"
-c = "Hello World!"
-
 print("Hello World!")
-print(c)
-print(f"{a} {b}")
---
+++ ruby/helloworld.rb
@@ -0,0 +1,6 @@
+"""
+2023/09/12
+Hello World
+"""
+
+print "Hello World"
